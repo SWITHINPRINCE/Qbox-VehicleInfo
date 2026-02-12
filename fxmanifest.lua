5f335b6958994edbd95d632225e8ac2337a96943
SWITHINPRINCE <swithinprince27502@gmail.com>
2026-02-12 11:05:59 +0530

    Created VehicleInfo FIle

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Prince'
+description 'Ox-lib Based Vehicle Info'
+version '1.0.0'
+lua54 'yes'
+
+client_scripts {
+    'client.lua'
+}
+
+shared_script {
+    '@ox_lib/init.lua'
+}
